c0b6234953780075724c224a7d093cae64844281
Emanuel Celayes <ecelayes@gmail.com>
2024-01-08 18:12:51 -0300

    Update config.js with environment variables

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 import { config } from 'dotenv';
 config();
 
-export const PORT = 8082;
-export const FRONT = 5173;
+export const PORT = process.env.BACK_PORT;
+export const FRONT = process.env.FRONT_PORT;
 export const HOST = `http://localhost:${FRONT}`;
 
 export const MP_TOKEN = process.env.MP_TOKEN;
